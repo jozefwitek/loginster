10670df319bbf9428ecad86efb18510ad6a65f9c
snuk0 <kiobelpriv@gmail.com>
2022-10-11 00:37:35 +0200

    Enhanced security :shield:

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,10 +12,25 @@ const testRouter = require('./routes/testRoutes');
 
 const app = express();
 
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-app.use(express.json());
+
+const limiter = rateLimit({
+    max: 100, // 100 requests
+    windowMs: 60 * 60 * 1000, // per 1h
+    message: 'Too many requests from this IP, please try again in hour!'
+});
+
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use(mongoSanitize());
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/users', userRouter);
